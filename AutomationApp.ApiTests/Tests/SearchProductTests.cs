5d8882a82769ba458c13488ff7d3c84ed3310aab
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-03-17 19:18:45 +0200

    Add Search Product tests

---
+++ AutomationApp.ApiTests/Tests/SearchProductTests.cs
@@ -0,0 +1,42 @@
+﻿using AutomationApp.ApiTests.Models;
+using AutomationApp.ApiTests.Models.Products;
+using AutomationApp.ApiTests.Utilities;
+using FluentAssertions;
+using RestSharp;
+using System.Net;
+
+namespace AutomationApp.ApiTests.Tests
+{
+    [Category("SearchProductApi")]
+    public class SearchProductTests: BaseTest
+    {
+        [TestCase("top")]
+        [TestCase("tshirt")]
+        [TestCase("jean")]
+        public async Task SearchProduct_WithValidSearchTerm_ReturnsProducts(string searchTerm)
+        {
+            var request = new RestRequest(ApiConstants.SearchProductEndpoint);
+            request.AddParameter("search_product", searchTerm);
+            var response = await Client.ExecutePostAsync<GetProductsResponse>(request);
+
+            AssertStatusCode(response, HttpStatusCode.OK);
+            response.Data.Should().NotBeNull();
+            response.Data!.Products.Should().NotBeEmpty();
+            response.Data.Products.Should().OnlyContain(p =>
+                p.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                p.Category.Category.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Test]
+        public async Task SearchProduct_WithoutSearchTerm_Returns400InResponseBody()
+        {
+            var request = new RestRequest(ApiConstants.SearchProductEndpoint);
+            var response = await Client.ExecutePostAsync<ErrorResponse>(request);
+
+            AssertStatusCode(response, HttpStatusCode.OK);
+            response.Data.Should().NotBeNull();
+            response.Data!.ResponseCode.Should().Be(400);
+            response.Data!.Message.Should().Be("Bad request, search_product parameter is missing in POST request.");
+        }
+    }
+}
--- AutomationApp.ApiTests/Utilities/ApiConstants.cs
+++ AutomationApp.ApiTests/Utilities/ApiConstants.cs
@@ -4,5 +4,6 @@
     {
         public const string ProductsListEndpoint = "productsList";
         public const string BrandsListEndpoint = "brandsList";
+        public const string SearchProductEndpoint = "searchProduct";
     }
 }
